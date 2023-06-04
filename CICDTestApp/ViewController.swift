9d603104f34d9e7aacf4ee7aa5c869e97ac82f5f
이준영 <junlight94@gmail.com>
2023-06-04 14:03:39 +0900

    Initial Commit

---
+++ CICDTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CICDTestApp
+//
+//  Created by Junyoung on 2023/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
